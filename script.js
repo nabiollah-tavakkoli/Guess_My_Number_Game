79eda10b435b6d092143fdccd09b10c5c48ae97b
N. Tavakkoli <n.tavakkoli@aeffegroup.biz>
2025-01-29 09:12:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+console.log(secretNumber);
+let score = 20;
+let highScore = 0;
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  console.log(secretNumber);
+  score = 20;
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.getElementById('guess-id').disabled = false;
+});
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      'Forgot to enter a number!';
+  } else if (guess === secretNumber) {
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    document.querySelector('.number').textContent = secretNumber;
+    if (highScore === 20) {
+      document.querySelector('body').style.backgroundColor = '#e67700';
+      document.querySelector('.message').textContent = 'BEST SCORE';
+    } else {
+      document.querySelector('body').style.backgroundColor = '#087f5b';
+      document.querySelector('.message').textContent = 'Correct Number 🎉🥳';
+    }
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too High 📈' : 'Too Low 📉';
+      document.querySelector('.score').textContent = --score;
+    } else {
+      document.querySelector('.message').textContent = 'GAME OVER!';
+      document.querySelector('.score').textContent = 0;
+      document.getElementById('guess-id').disabled = true;
+    }
+  }
+});
